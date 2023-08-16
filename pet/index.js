4a3260499f1852ca4407bbe2a9eaa934c7e8eb8c
Aejin Kang <dowlsdl003@naver.com>
2023-08-16 17:43:10 +0900

    메인 페이지 완성, 기존 메인페이지를 pet페이지로 옮김

---
+++ pet/index.js
@@ -0,0 +1,286 @@
+let currentPage = 0; //현재 페이지 번호
+let isLastPage = false; //마지막 페이지 인지 여부 
+const MAX_MEMO = 4;// 고정된 메모 갯수
+let currentQuery = ""; // 현재 검색 키워드
+
+
+(()=> {
+  const divs = document.querySelectorAll("div");
+  const buttons = divs[1].querySelectorAll("button");
+
+  buttons[0].addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.replace("http://localhost:5500/login.html");
+  })
+  buttons[1].addEventListener("click", (e) => {
+    e.preventDefault();
+    //토큰 만료 시킨 후
+    //메인페이지 보여줌
+    window.location.reload;
+  })
+})();
+
+//메모 형태
+function cardTemplate(item){
+  const imageElement = item.image ? `<img src="${item.image}" alt="반려동물사진">` : "";
+  const template =  /*html*/
+    `<article data-no="${item.no}">
+    <div>
+    <h4 title="제목을 누르면 상세페이지로 이동합니다.">${item.title}</h4>
+    <button class="remove">X</button>
+    </div>
+    <hr>
+    <h5><sub>생성시간: ${new Date(item.createdTime).toLocaleString()}</sub></h5>
+    <div>
+    ${imageElement}
+    </div>
+    <div>
+    <p>${item.content}</p>
+    <button class="btn-modify">:</button>
+    </div>
+    <hr>
+    <div>
+    <h3>${item.petname}</h3>
+    <h3>${item.nickname}</h3>
+    </div>
+    </article>`;
+    return template;
+}
+
+//메모를 찾아서 조회 후 화면에 보이는 메서드
+async function getPagedMemo(page, query){
+  const section = document.querySelector("section");
+  let url = "";
+  if(query){
+    url = `http://localhost:8080/posts/paging/search?page=${page}&size=${MAX_MEMO}&query=${query}`
+  }else {
+    url = `http://localhost:8080/posts/paging?page=${page}&size=${MAX_MEMO}`;
+  }
+  const response = await fetch(url);
+
+  const results = await response.json();
+
+  //목록 초기화
+  section.innerHTML = "";
+  results.content.forEach(item => {
+    section.insertAdjacentHTML("beforeend", cardTemplate(item));
+  });
+
+  currentPage = results.number;
+  isLastPage = results.last;
+  //페이징 버튼
+  setBtnActive();
+}
+
+//화면을 처음 켰을 때 첫번째 페이지 조회
+(async() => {
+  window.addEventListener("DOMContentLoaded", () => {
+    getPagedMemo(0);
+  });
+})();
+
+//검색 기능
+(() => {
+  const textQuery = document.forms[0].querySelector("input");
+  const btnSearch = document.forms[0].querySelector("button");
+
+  console.log(textQuery);
+  console.log(btnSearch);
+
+  btnSearch.addEventListener("click", (e) => {
+    e.preventDefault();
+    currentQuery = textQuery.value;
+    getPagedMemo(0, currentQuery);
+  });
+
+  textQuery.addEventListener("keyup", (e) => {
+    e.preventDefault();
+    if(e.key.toLowerCase() === "enter"){
+      currentQuery = textQuery.value;
+      getPagedMemo(0, currentQuery);
+    }
+  });
+})();
+
+//검색 조건 초기화
+(() => {
+  const btnReset = document.forms[0].querySelectorAll("button")[0];
+  btnReset.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.forms[0].reset();
+    currentQuery = "";
+    getPagedMemo(0, currentQuery);
+  });
+})();
+
+//삭제
+(() => {
+  const section = document.querySelector("section");
+
+  section.addEventListener("click", async(e) => {
+    e.preventDefault();
+
+    if(e.target.classList.contains("remove")){
+
+      const removeArticle = e.target.closest("article");
+      const removeNumber = removeArticle.dataset.no;
+      
+      //서버연결
+      await fetch(`http://localhost:8080/posts/${removeNumber}`,
+      {
+        method: "DELETE",
+        // headers: {
+        //   Authorization: `Bearer ${getCookie(
+        //     "token"
+        //   )}`,
+        // },
+      });
+      removeArticle.remove();
+      window.location.reload();
+      
+    }
+    
+  })
+
+})();
+
+//수정
+(() => {
+  document.querySelector("section").addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    if(e.target.classList.contains("btn-modify")){
+      const modifyArticle = e.target.closest("article");
+      console.log(modifyArticle);
+     
+      //레이어 띄우기
+      /** @type {HTMLDivElement} */
+      const layer = document.querySelector("#modify-layer");
+      layer.hidden = false;
+
+      const title = modifyArticle.querySelector("h4");
+      layer.querySelector("input").value = title.innerHTML;
+
+      const textbox = modifyArticle.querySelector("p");
+      layer.querySelector("textarea").value = textbox.innerHTML;
+
+      const buttons = layer.querySelectorAll("button");
+
+      buttons[1].addEventListener("click", (e) => {
+        e.preventDefault();
+        layer.hidden = true;
+      });
+
+
+      //수정
+      buttons[0].addEventListener("click", async(e) => {
+        e.preventDefault();
+
+        const modifyNum = modifyArticle.dataset.no;
+        
+        const inputs = layer.querySelectorAll("input");
+        const modifyTitle = inputs[0].value;
+        const modifyTextbox = layer.querySelector("textarea").value;
+        const file = inputs[1];
+
+      
+        async function modifyPost(image) {
+          await fetch (
+            `http://localhost:8080/posts/${modifyNum}` , {
+            method: "PUT",
+            headers: {
+              "content-type": "application/json",
+            },
+            body: JSON.stringify ({
+              title: modifyTitle,
+              content: modifyTextbox,
+              image: image ? image : null,
+            }),
+          });
+        }
+        
+        console.log(file.files[0]);
+        if(file.files[0]){
+          const reader = new FileReader();
+          
+          reader.addEventListener("load", 
+          async(e) => {
+          console.log(e);
+          const image = e.target.result;
+          modifyPost(image);
+          const imageElement = modifyArticle.querySelector("img");
+          imageElement.src = image;
+        });
+        reader.readAsDataURL(file.files[0]);
+        }else {modifyPost();}
+
+        console.log(file.files[0]);
+        
+        title.innerHTML = layer.querySelector("input").value;
+        textbox.innerHTML = layer.querySelector("textarea").value;
+        
+        
+        layer.hidden = true;
+      })
+
+      
+    }
+
+  });
+})();
+
+//페이징 버튼
+function setBtnActive() {
+  //버튼 선택
+  const paging = document.getElementById("paging");
+  const buttons = paging.querySelectorAll("button");
+
+  const btnPrev = buttons[0];
+  const btnNext = buttons[1];
+
+  if(currentPage === 0){
+    btnPrev.disabled = true;
+  }else{
+    btnPrev.disabled = false;
+  }
+ 
+  if(isLastPage) {
+    btnNext.disabled = true;
+  }else{
+    btnNext.disabled = false;
+  }
+}
+
+//페이징
+(() => {
+  const paging = document.getElementById("paging");
+  const buttons = paging.querySelectorAll("button");
+
+  const btnPrev = buttons[0];
+  const btnNext = buttons[1];
+  console.log(btnPrev);
+  console.log(btnNext);
+
+  //이전 버튼
+  btnPrev.addEventListener("click", (e) => {
+    e.preventDefault();
+    currentPage > 0 && getPagedMemo(currentPage -1, currentQuery);
+  });
+  //다음 버튼
+  btnNext.addEventListener("click", (e) => {
+    e.preventDefault();
+    !isLastPage && getPagedMemo(currentPage + 1, currentQuery);
+  });
+})();
+
+//상세페이지 이동
+(() => {
+  const section = document.querySelector("section");
+  section.addEventListener("click", (e) => {
+    e.preventDefault();
+    
+    if(e.target.tagName.toLowerCase() === "h4"){
+      window.location.href ="pet/details.html";
+    }
+  })
+})();
