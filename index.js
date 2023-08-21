1c8f3ce68565f71679ef7bf02a78c1b30a96f26c
Aejin Kang <dowlsdl003@naver.com>
2023-08-21 20:30:47 +0900

    reservation getmapping 구현중..

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 (() => {
   hiddenButton();
   loginLogout();
+  
 })();
 
 (() => {
@@ -10,4 +11,23 @@
       rotateElement[i].classList.add("rotate-animate");
     }
   });
+})();
+
+(async() => {
+  const currentDay = new Date().getTime();
+  console.log(currentDay);
+  const response = await fetch(`http://localhost:8080/reserve/${currentDay}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${getCookie(
+        "token"
+      )}`,
+    },
+  });
+  const result = response.json();
+  console.log(result);
+  if([202].includes(response.status)){
+    alert("병원 예약일입니다.");
+  }
+
 })();
