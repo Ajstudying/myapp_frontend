dba56a9b3ac88a9a3d68fa7dd548d685086bca33
Aejin Kang <dowlsdl003@naver.com>
2023-08-25 15:45:03 +0900

    동일하게 적용되는 header css 만듬. 게시판 작성페이지, 수정페이지 통합 완료

--- board/board.js
+++ board/board.js
@@ -1,55 +1,62 @@
 let currentPage = 0; //현재 페이지 번호
-let isLastPage = false; //마지막 페이지 인지 여부 
-const MAX_PAGE = 5;// 고정된 페이지 갯수
+let isLastPage = false; //마지막 페이지 인지 여부
+const MAX_PAGE = 5; // 고정된 페이지 갯수
 let currentQuery = ""; // 현재 검색 키워드
 
 (() => {
   const token = getCookie("token");
   console.log(token);
-  if(!token){
-    window.location.href = "http://localhost:5500/auth/login.html"
+  if (!token) {
+    window.location.href = "http://localhost:5500/auth/login.html";
   }
   hiddenButton();
   loginLogout();
 })();
 
 //게시글 모양
-async function getPagedBoard(page, option, query){
+async function getPagedBoard(page, option, query) {
   const section = document.querySelectorAll("section")[0];
-    let url = "";
-    if(query) {
-      url = `http://localhost:8080/boards/paging/search?page=${page}&size=${MAX_PAGE}&${option}=${query}`
-    }else{
-      url = `http://localhost:8080/boards/paging?page=${page}&size=${MAX_PAGE}`;
-    }
+  let url = "";
+  if (query) {
+    url = `http://localhost:8080/boards/paging/search?page=${page}&size=${MAX_PAGE}&${option}=${query}`;
+  } else {
+    url = `http://localhost:8080/boards/paging?page=${page}&size=${MAX_PAGE}`;
+  }
 
-    const response = await fetch(url, {
-      headers: {
-        Authorization: `Bearer ${getCookie(
-          "token"
-        )}`,
-      },
-    });
-    if ([401, 403].includes(response.status)) {
-      // 로그인 페이지로 튕김
-      alert("인증처리가 되지 않았습니다.");
-      window.location.href = "http://localhost:5500/auth/login.html";
-    }
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${getCookie("token")}`,
+    },
+  });
+  if ([401, 403].includes(response.status)) {
+    // 로그인 페이지로 튕김
+    alert("인증처리가 되지 않았습니다.");
+    window.location.href = "http://localhost:5500/auth/login.html";
+  }
 
-    const results = await response.json();
+  const results = await response.json();
 
-    //목록초기화
-    section.innerHTML = "";
-    const result = results.content;
-    const ul = document.createElement("ul");
-    section.append(ul);
-    result.forEach(item => {
-      const time= new Date(item.createdTime);
-      const reservationTime = `${time.getFullYear()}-${time.getMonth()}-${time.getDate().toString()}`;
-      const li = document.createElement("li");
-      const hr = document.createElement("hr");
-      const template = /*html*/
-      `<div><sub>${item.species}</sub></div>
+  //목록초기화
+  section.innerHTML = "";
+  const result = results.content;
+  const ul = document.createElement("ul");
+  section.append(ul);
+  result.forEach((item) => {
+    const time = new Date(item.createdTime);
+    const reservationTime = `${time.getFullYear()}-${time.getMonth()}-${time
+      .getDate()
+      .toString()}`;
+    const li = document.createElement("li");
+    const hr = document.createElement("hr");
+    const request =
+      item.request === "inquiry"
+        ? "문의"
+        : item.request === "recommend"
+        ? "추천"
+        : "";
+    const template =
+      /*html*/
+      `<div><sub>${item.species}</sub><button data-request="${item.request}">${request}</button></div>
       <div>
       <span>
       <p class="title">${item.title}</p>
@@ -59,15 +66,15 @@ async function getPagedBoard(page, option, query){
       <p>${reservationTime}</p>
       </span>
       </div>`;
-      li.dataset.no = item.no;
-      li.insertAdjacentHTML("afterbegin", template);
-      ul.append(li);
-      ul.append(hr);
-    });
+    li.dataset.no = item.no;
+    li.insertAdjacentHTML("afterbegin", template);
+    ul.append(li);
+    ul.append(hr);
+  });
 
   currentPage = results.number; //현재 페이지 설정
   isLastPage = results.last; // 마지막 페이지 여부
-      
+
   //이전 /다음 버튼 활성화 처리
   setBtnActive();
 }
@@ -84,14 +91,14 @@ async function getPagedBoard(page, option, query){
   const section = document.querySelector("section");
   section.addEventListener("click", (e) => {
     e.preventDefault();
-    
-    if(e.target.classList.contains("title")){
+
+    if (e.target.classList.contains("title")) {
       const li = e.target.closest("li");
       const boardNo = li.dataset.no;
 
       window.location.href = `http://localhost:5500/board/details.html?boardNo=${boardNo}`;
     }
-  })
+  });
 })();
 
 function setBtnActive() {
@@ -102,18 +109,17 @@ function setBtnActive() {
   const btnNext = buttons[1];
 
   //첫번째 페이지면 이전 버튼 비활성화
-  if(currentPage === 0){
+  if (currentPage === 0) {
     btnPrev.disabled = true;
-  }else{
+  } else {
     btnPrev.disabled = false;
   }
   //마지막 페이지면 다음 버튼 비활성화
-  if(isLastPage) {
+  if (isLastPage) {
     btnNext.disabled = true;
-  }else{
+  } else {
     btnNext.disabled = false;
   }
-
 }
 
 //페이징
@@ -127,7 +133,7 @@ function setBtnActive() {
   //이전 버튼
   btnPrev.addEventListener("click", (e) => {
     e.preventDefault();
-    currentPage > 0 && getPagedBoard(currentPage -1, currentQuery);
+    currentPage > 0 && getPagedBoard(currentPage - 1, currentQuery);
   });
   //다음 버튼
   btnNext.addEventListener("click", (e) => {
@@ -140,7 +146,9 @@ function setBtnActive() {
 (() => {
   const option = document.querySelector("select");
   const textQuery = document.querySelector("input");
-  const buttons = document.querySelectorAll("section")[2].querySelectorAll("button");
+  const buttons = document
+    .querySelectorAll("section")[2]
+    .querySelectorAll("button");
   const btnSearch = buttons[0];
 
   btnSearch.addEventListener("click", (e) => {
@@ -151,22 +159,88 @@ function setBtnActive() {
 
   textQuery.addEventListener("keyup", (e) => {
     e.preventDefault();
-    if(e.key.toLowerCase() === "enter"){
+    if (e.key.toLowerCase() === "enter") {
       currentQuery = textQuery.value;
       getPagedBoard(0, option.value, currentQuery);
     }
   });
-
 })();
 
 //검색조건 초기화
 (() => {
-  const btnReset = document.querySelectorAll("section")[2].querySelectorAll("button")[1];
+  const btnReset = document
+    .querySelectorAll("section")[2]
+    .querySelectorAll("button")[1];
 
   btnReset.addEventListener("click", (e) => {
     e.preventDefault();
     document.forms[0].reset();
     currentQuery = "";
     getPagedBoard(0, currentQuery);
+  });
+})();
+
+//문의/추천 등 글자를 눌렀을 때 해당 것만 나오게 검색 페이지 기능.
+(() => {
+  const section = document.querySelector("section");
+
+  section.addEventListener("click", async (e) => {
+    e.preventDefault();
+    if (e.target.tagName.toLowerCase() == "button") {
+      const request = e.target.dataset.request;
+      console.log(request);
+
+      const response = await fetch(
+        `http://localhost:8080/boards/paging/request?page=0&size=${MAX_PAGE}&request=${request}`,
+        {
+          headers: {
+            Authorization: `Bearer ${getCookie("token")}`,
+          },
+        }
+      );
+      const results = await response.json();
+
+      //목록초기화
+      section.innerHTML = "";
+      const result = results.content;
+      const ul = document.createElement("ul");
+      section.append(ul);
+      result.forEach((item) => {
+        const time = new Date(item.createdTime);
+        const reservationTime = `${time.getFullYear()}-${time.getMonth()}-${time
+          .getDate()
+          .toString()}`;
+        const li = document.createElement("li");
+        const hr = document.createElement("hr");
+        const request =
+          item.request === "inquiry"
+            ? "문의"
+            : item.request === "recommend"
+            ? "추천"
+            : "";
+        const template =
+          /*html*/
+          `<div><sub>${item.species}</sub><button data-request="${item.request}">${request}</button></div>
+      <div>
+      <span>
+      <p class="title">${item.title}</p>
+      </span>
+      <span>
+      <p>작성자: ${item.nickname}</p>
+      <p>${reservationTime}</p>
+      </span>
+      </div>`;
+        li.dataset.no = item.no;
+        li.insertAdjacentHTML("afterbegin", template);
+        ul.append(li);
+        ul.append(hr);
+      });
+
+      currentPage = results.number; //현재 페이지 설정
+      isLastPage = results.last; // 마지막 페이지 여부
+
+      //이전 /다음 버튼 활성화 처리
+      setBtnActive();
+    }
   });
 })();
--- board/modify-write.js
+++ board/modify-write.js
@@ -3,173 +3,215 @@
   loginLogout();
   const token = getCookie("token");
   console.log(token);
-  if(!token){
-    window.location.href = "http://localhost:5500/auth/login.html"
+  if (!token) {
+    window.location.href = "http://localhost:5500/auth/login.html";
   }
-
 })();
 
 //셀렉트 옵션 추가
-(async() => {
+(async () => {
   const select = document.forms[0].querySelectorAll("select")[1];
 
   console.log(select);
   const url = "http://localhost:8080/profile";
   const response = await fetch(url, {
     headers: {
-      Authorization: `Bearer ${getCookie(
-        "token"
-      )}`,
+      Authorization: `Bearer ${getCookie("token")}`,
     },
   });
   const result = await response.json();
 
   console.log(result);
 
-  result.data.forEach(item => {
-    const option =  /*html*/
-    `<option data-pet="${item[3]}"value="${item[2]}">${item[2]}</option>`;
+  result.data.forEach((item) => {
+    const option =
+      /*html*/
+      `<option data-pet="${item[3]}"value="${item[2]}">${item[2]}</option>`;
     select.insertAdjacentHTML("beforeend", option);
   });
-
 })();
 
-(() => {
-  
-})();
+//수정으로 받아온 쿼리 있을 때
+(async () => {
+  // URL에서 쿼리 파라미터 가져오기
+  const urlParams = new URLSearchParams(window.location.search);
+  const boardNo = urlParams.get("boardNo");
 
-//수정으로 쿼리를 받아올 때
-(async() => {
-    // URL에서 쿼리 파라미터 가져오기
-    const urlParams = new URLSearchParams(window.location.search);
-    const boardNo = urlParams.get('boardNo'); // 쿼리 파라미터에서 boardNo 값을 가져옴
-
-    const form = document.querySelector("form");
-    const title = form.querySelectorAll("input")[0];
-    const petname = form.querySelectorAll("select")[1];
-    const content = form.querySelector("textarea");
-    const file = form.querySelectorAll("input")[1];
-
-    async function modifyBoard(image) {
-      await fetch(`http://localhost:8080/boards/${boardNo}`, {
-      method: "PUT",
+  const section = document.querySelector("section");
+  const button = section.querySelectorAll("div")[1].querySelector("button");
+  if (boardNo) {
+    const response = await fetch(`http://localhost:8080/boards/${boardNo}`, {
+      method: "GET",
       headers: {
-        "content-type": "application/json",
-        "Authorization": `Bearer ${getCookie(
-        "token"
-        )}`,
+        Authorization: `Bearer ${getCookie("token")}`,
       },
-      body: JSON.stringify ({
-        title: title.value,
-        content: content.value,
-        petname: petname.value,
-        image: image ? image : null,
-      }),
     });
-    }
-    if(file.files[0]){
-      const reader = new FileReader();
-      reader.addEventListener("load", async(e) => {
-        const image = e.target.result;
-        modifyBoard(image);
-      });
-    }else{modifyBoard();}
+
+    const form = section.querySelector("form");
+    const title = section.querySelectorAll("input")[0];
+    const textbox = section.querySelector("textarea");
+    const select = document.forms[0].querySelectorAll("select")[0];
+    const petname = document.forms[0].querySelectorAll("select")[1];
+    const file = section.querySelectorAll("input")[1];
+
+    button.classList.add("modify");
+    button.innerHTML = "수정";
+
+    const result = await response.json();
+    form.dataset.nickname = result.nickname;
+    select.value = result.request;
+    title.value = result.title;
+    petname.value = result.petname;
+    textbox.value = result.content;
+    file.src = result.image ? result.image : "";
+  } else {
+    button.classList.add("add");
+    button.innerHTML = "저장";
+  }
 })();
 
+//수정으로 쿼리를 받아서, 수정 버튼을 눌렀을 때 수정저장되도록
+(() => {
+  const modifyBtn = document.forms[0].querySelector("button");
 
+  modifyBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    if (e.target.classList.contains("modify")) {
+      // URL에서 쿼리 파라미터 가져오기
+      const urlParams = new URLSearchParams(window.location.search);
+      const boardNo = urlParams.get("boardNo"); // 쿼리 파라미터에서 boardNo 값을 가져옴
+
+      const form = document.querySelector("form");
+      const title = form.querySelectorAll("input")[0];
+      const select = form.querySelectorAll("select")[0];
+      const petname = form.querySelectorAll("select")[1];
+      const species = petname[petname.selectedIndex].dataset.pet;
+      const content = form.querySelector("textarea");
+      const file = form.querySelectorAll("input")[1];
+      console.log(species);
+      console.log(file.src);
+
+      async function modifyBoard(image) {
+        await fetch(`http://localhost:8080/boards/${boardNo}`, {
+          method: "PUT",
+          headers: {
+            "content-type": "application/json",
+            Authorization: `Bearer ${getCookie("token")}`,
+          },
+          body: JSON.stringify({
+            title: title.value,
+            content: content.value,
+            request: select.value,
+            image: image ? image : null,
+            species: species,
+            petname: petname.value,
+          }),
+        });
+      }
+      if (file.files[0]) {
+        const reader = new FileReader();
+        reader.addEventListener("load", async (e) => {
+          const image = e.target.result;
+          modifyBoard(image);
+          alert("수정이 완료되었습니다.");
+          window.location.href = `http://localhost:5500/board/details.html?boardNo=${boardNo}`;
+        });
+        reader.readAsDataURL(file.files[0]);
+      } else if (file.src) {
+        const image = file.src;
+        modifyBoard(image);
+        alert("수정이 완료되었습니다.");
+        window.location.href = `http://localhost:5500/board/details.html?boardNo=${boardNo}`;
+      } else {
+        modifyBoard();
+        alert("수정이 완료되었습니다.");
+        window.location.href = `http://localhost:5500/board/details.html?boardNo=${boardNo}`;
+      }
+    }
+  });
+})();
 
 //추가
-function addBoard() {
-
+(() => {
   //데이터를 추가하기 위해 엘레멘트 찾기
   const inputs = document.forms[0].querySelectorAll("input");
   const textbox = document.forms[0].querySelector("textarea");
-  const select = document.forms[0].querySelector("select");
+  const petname = document.forms[0].querySelectorAll("select")[1];
+  const select = document.forms[0].querySelectorAll("select")[0];
 
   const title = inputs[0];
   const content = textbox;
   const file = inputs[1];
-  const petname = select;
 
   const add = document.forms[0].querySelector("button");
-
   add.addEventListener("click", (e) => {
     e.preventDefault();
+    if (e.target.classList.contains("add")) {
+      if (select.value == "선택") {
+        alert("게시글의 종류를 선택해주세요.");
+        return;
+      }
+      if (title.value === "") {
+        alert("제목을 입력해주세요.");
+        return;
+      }
 
-    if(title.value === ""){
-      alert("제목을 입력해주세요.");
-      return;
-    }
+      if (content.value === "") {
+        alert("내용을 입력해주세요.");
+        return;
+      }
 
-    if(content.value === ""){
-      alert("내용을 입력해주세요.");
-      return;
-    }
-    
-    if(petname.value === "반려동물을 선택해주세요"){
-      alert("반려동물을 선택해주세요.");
-      return;
-    }
+      if (petname.value === "반려동물") {
+        alert("반려동물을 선택해주세요.");
+        return;
+      }
 
-    //데이터를 서버에 전송하고, UI 요소 생성
-    async function createBoard(image) {
-      const species = select[select.selectedIndex].dataset.pet;
-      console.log(species);
-      //서버에 전송하면 UI를 생성한다.
-      const response = await fetch(
-        "http://localhost:8080/boards",
-        {
+      //데이터를 서버에 전송하고, UI 요소 생성
+      async function createBoard(image) {
+        const species = petname[petname.selectedIndex].dataset.pet;
+        console.log(species);
+        //서버에 전송하면 UI를 생성한다.
+        const response = await fetch("http://localhost:8080/boards", {
           method: "POST",
           headers: {
             "content-type": "application/json",
-            "Authorization": `Bearer ${getCookie(
-              "token"
-            )}`,
+            Authorization: `Bearer ${getCookie("token")}`,
           },
           body: JSON.stringify({
             title: title.value,
             content: content.value,
             image: image ? image : null,
+            request: select.value,
             species: species,
             petname: petname.value,
           }),
-        }
-      );
-      console.log(response);
+        });
+        console.log(response);
+      }
 
-    }
+      if (file.files[0]) {
+        //파일이 있을 때
+        //파일 읽은 후 이벤트 실행
+        const reader = new FileReader();
 
-    if (file.files[0]) {
-      //파일이 있을 때
-      //파일 읽은 후 이벤트 실행
-      const reader = new FileReader();
-
-      reader.addEventListener(
-        "load", 
-        async(e) => {
-          console.log(e);
+        reader.addEventListener("load", async (e) => {
           const image = e.target.result;
           createBoard(image);
-        }
-      );
-      reader.readAsDataURL(file.files[0]);
-      const section = document.querySelector("section");
-      const form = section.querySelector("form");
-      form.hidden = true;
-      section.innerHTML = /*html*/
-      `<p> 작성이 완료되었습니다. </p>`;
-    }else {
-      //파일이 없을 때
-      createBoard();
-      const section = document.querySelector("section");
-      const form = section.querySelector("form");
-      form.hidden = true;
-      section.innerHTML = /*html*/
-      `<p> 작성이 완료되었습니다. </p>`;
-      
+        });
+        reader.readAsDataURL(file.files[0]);
+        const section = document.querySelector("section");
+        const form = section.querySelector("form");
+        form.hidden = true;
+        section.innerHTML = /*html*/ `<p> 작성이 완료되었습니다. </p>`;
+      } else {
+        //파일이 없을 때
+        createBoard();
+        const section = document.querySelector("section");
+        const form = section.querySelector("form");
+        form.hidden = true;
+        section.innerHTML = /*html*/ `<p> 작성이 완료되었습니다. </p>`;
+      }
     }
-
   });
-
-}
-
+})();
