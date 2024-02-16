590f398edda41582aefb0dd5f881561fd5021c03
Ajstudying <rkddowls1@gmail.com>
2024-02-16 16:01:39 +0900

    정보나눔 게시판 검색창 위치 수정/반응에 따라 마진 수정

--- board/board.js
+++ board/board.js
@@ -1,6 +1,6 @@
 let currentPage = 0; //현재 페이지 번호
 let isLastPage = false; //마지막 페이지 인지 여부
-const MAX_PAGE = 5; // 고정된 페이지 갯수
+const MAX_PAGE = 4; // 고정된 페이지 갯수
 let currentQuery = ""; // 현재 검색 키워드
 
 (() => {
